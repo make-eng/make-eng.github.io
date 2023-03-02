7b786c75c4448d260ca52059c20d071f23923074
qizhang <13681466643@163.com>
2023-03-02 17:58:20 +0800

    feat: home

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -14,6 +14,8 @@ git add -A
 git commit -m "deploy"
 git branch -M main
 git remote add origin git@github.com:Pancake-Q/Pancake-Q.github.io.git
+git config --local user.name "Pancake-Q"
+git config --local user.email "pancakeqi.code@gmail.com"
 git push -f git@github.com:Pancake-Q/Pancake-Q.github.io.git main:gh-page
 
 cd -
